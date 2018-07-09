a2aacf01aa34a4e479ed14a074da4be030cbd15c
khalid yousafzai <khalidyousafzai@comsats.edu.pk>
2018-07-09 17:12:10 +0500

    laplacian

---
+++ fig/figPartialDifferentialEquations/figOctaveCircularMembraneNormalModes.m
@@ -0,0 +1,32 @@
+diffBessel=@(x) 0.5.*(besselj(0,x)-besselj(2,x));
+
+#=========
+
+kread=fopen("figOctaveCircularMembraneNormalModesOne.dat","w");
+
+for x=[-2.4048:0.1:2.4048];
+fprintf(kread,"%i %s %i",x,"    ",besselj(0,x)); fprintf(kread,"\n")
+endfor
+
+fclose(kread)
+#=======================
+#=========
+
+kread=fopen("figOctaveCircularMembraneNormalModesTwo.dat","w");
+
+for x=[-5.5201:0.1:5.5201];
+fprintf(kread,"%i %s %i",x,"    ",besselj(0,x)); fprintf(kread,"\n")
+endfor
+
+fclose(kread)
+#=======================
+#=========
+
+kread=fopen("figOctaveCircularMembraneNormalModesThree.dat","w");
+
+for x=[-8.6537:0.1:8.6537];
+fprintf(kread,"%i %s %i",x,"    ",besselj(0,x)); fprintf(kread,"\n")
+endfor
+
+fclose(kread)
+#=======================
